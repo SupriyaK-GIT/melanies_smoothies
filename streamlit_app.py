dca528f25eda6651f16d9fffe87f4dc99ff8e0d5
SupriyaK-GIT <learningbysk@gmail.com>
2024-10-12 23:18:03 +0530

    Update streamlit_app.py
    Connected changes needed bc to move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+###from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 
@@ -11,8 +11,8 @@ st.write(
     """Orders that need to be filled
     """
 )
-
-session = get_active_session()
+cnx=st.connection('Snowflake')
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 if my_dataframe:
