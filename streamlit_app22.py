9d62d1622af914ad636d2270b4e3e93aa5c2d91b
SupriyaK-GIT <learningbysk@gmail.com>
2024-10-13 15:30:21 +0530

    Update streamlit_app22.py

--- streamlit_app22.py
+++ streamlit_app22.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 
 
@@ -45,6 +46,5 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, Igor!', icon="✅")
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
