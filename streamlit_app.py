05b6a041cd3cfc06906612e249e0e59de36443c4
SupriyaK-GIT <learningbysk@gmail.com>
2024-10-13 15:01:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,11 @@
 # streamlit_app.py
-
 import streamlit as st
-
-
 
 st.title(" :cup_with_straw: Pending Smoothie orders :cup_with_straw:")
 st.write(
     """Orders that need to be filled testing 
     """
 )
-
-
-
 # Snowflake connection parameters
 snowflake = {
     'account': 'NONGWKL-MOB76819',
@@ -22,3 +16,8 @@ snowflake = {
     'database': 'SMOOTHIES',  # Optional
     'schema': 'PUBLIC',  # Optional
 }
+
+# Initialize connection.
+conn = st.connection("snowflake")
+
+
