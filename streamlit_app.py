0333f32ee819ecb1b1f600ff90de161de481f15c
SupriyaK-GIT <learningbysk@gmail.com>
2024-10-13 15:01:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,16 @@
+# streamlit_app.py
+
 import streamlit as st
+
+
 
 st.title(" :cup_with_straw: Pending Smoothie orders :cup_with_straw:")
 st.write(
     """Orders that need to be filled testing 
     """
 )
+
+
 
 # Snowflake connection parameters
 snowflake = {
@@ -16,6 +22,16 @@ snowflake = {
     'database': 'SMOOTHIES',  # Optional
     'schema': 'PUBLIC',  # Optional
 }
+# Initialize connection.
+conn = st.connection("snowflake")
 
-cnx=st.connection('snowflake')
+# Perform query.
+df = conn.query("SELECT * from mytable;", ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.NAME} has a :{row.PET}:")
+
+
+
 
