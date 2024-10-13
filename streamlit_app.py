477dd0e422bfb4766e4bea2453d5b2e6185b3c42
SupriyaK-GIT <learningbysk@gmail.com>
2024-10-13 15:01:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,16 +22,3 @@ snowflake = {
     'database': 'SMOOTHIES',  # Optional
     'schema': 'PUBLIC',  # Optional
 }
-# Initialize connection.
-conn = st.connection("snowflake")
-
-# Perform query.
-df = conn.query("SELECT * from mytable;", ttl=600)
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.NAME} has a :{row.PET}:")
-
-
-
-
