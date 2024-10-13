fb41985ca8962ee2ea01ddf18d8f8ef34b3bc541
SupriyaK-GIT <learningbysk@gmail.com>
2024-10-13 15:29:21 +0530

    Update streamlit_app22.py

--- streamlit_app22.py
+++ streamlit_app22.py
@@ -32,7 +32,7 @@ ingredients_string=''
 for fruit_chosen in ingredients_list:
      ingredients_string+=fruit_chosen+' '
 
-st.write (ingredients_string)
+#st.write (ingredients_string)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -43,7 +43,7 @@ time_to_insert=st.button ('Submit Order');
 
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered!', icon="✅")
+    st.success('Your Smoothie is ordered, Igor!', icon="✅")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
